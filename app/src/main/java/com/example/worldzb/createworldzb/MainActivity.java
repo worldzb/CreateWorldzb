5fde0ae45cfb0d7a0438f0aeb81ea6b82888b87f
worldzb <yang641367741@gmail.com>
2017-11-07 00:57:16 +0800

    摄像机接口测试

--- app/src/main/java/com/example/worldzb/createworldzb/MainActivity.java
+++ app/src/main/java/com/example/worldzb/createworldzb/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.worldzb.createworldzb;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +12,9 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void startCamera(View view){
+        Intent intent=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivity(intent);
     }
 }
